2d4ac2dc7949c2b0e0a56b118076788e50924fe2
Domenico Ceccarelli <37450907+riddik14@users.noreply.github.com>
2025-08-08 02:32:46 +0200

    Create avvia_entrambi.bat

---
+++ avvia_entrambi.bat
@@ -0,0 +1,4 @@
+@echo off
+PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File "C:\Users\admin\Desktop\vittoriain.ps1"
+PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File "C:\Users\admin\Desktop\midiword.ps1"
+pause
